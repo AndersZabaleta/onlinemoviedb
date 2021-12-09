9b0dcc7c66c564e485aa0ae582330ab81bb82618
Ander Salas <ander.s.zabaleta@hotmail.com>
2021-12-09 19:44:41 +0100

    Destructuring data

--- src/components/movieDetails/movieDetails.js
+++ src/components/movieDetails/movieDetails.js
@@ -2,7 +2,7 @@ import { useParams, Navigate } from "react-router-dom";
 import useMovieDetailsQuery from "./services/useMovieDetailsQuery";
 import MovieCast from "./movieCast";
 import DetailedInfo from "../../shared/components/detailedInfo";
-import { MovieDetailsPage } from "../../shared/detailView.styled";
+import { DetailPage } from "../../shared/common/styles";
 const MovieDetails = () => {
   const { id } = useParams();
   const { isLoading, error, data } = useMovieDetailsQuery(id);
@@ -10,22 +10,33 @@ const MovieDetails = () => {
   if (isLoading) return "Loading...";
   if (error) return "There was an error " + error.message;
   if (data.status_code) return <Navigate to="/404" />;
+  const {
+    poster_path,
+    title,
+    overview,
+    release_date,
+    vote_average,
+    vote_count,
+    popularity,
+    runtime,
+    genres,
+  } = data;
   return (
-    <MovieDetailsPage>
+    <DetailPage>
       <DetailedInfo
         infoType="movie"
-        infoImg={data.poster_path}
-        name={data.title}
-        description={data.overview}
-        releaseDate={data.release_date}
-        voteAverage={data.vote_average}
-        voteCount={data.vote_count}
-        popularity={data.popularity}
-        runtime={data.runtime}
-        genres={data.genres}
+        infoImg={poster_path}
+        name={title}
+        description={overview}
+        releaseDate={release_date}
+        voteAverage={vote_average}
+        voteCount={vote_count}
+        popularity={popularity}
+        runtime={runtime}
+        genres={genres}
       />
       <MovieCast id={id} />
-    </MovieDetailsPage>
+    </DetailPage>
   );
 };
 
