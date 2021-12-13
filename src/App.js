ef52fa3065ba3fafb0bf5439a574133c0264f4b0
Ander Salas <ander.s.zabaleta@hotmail.com>
2021-12-13 17:26:10 +0100

    Refactor: lazy import components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,63 @@
 import { Routes, Route } from "react-router-dom";
-import {
-  Header,
-  Movies,
-  MovieDetails,
-  Home,
-  NotFound,
-  PeopleDetails,
-} from "./components";
+import { Suspense, lazy } from "react";
+import Header from "./components/header";
 import "./App.css";
+const Home = lazy(() => import("./components/home"));
+const Movies = lazy(() => import("./components/movies"));
+const MovieDetails = lazy(() => import("./components/movieDetails"));
+const PeopleDetails = lazy(() => import("./components/peopleDetails"));
+const NotFound = lazy(() => import("./components/notFound"));
 
 const App = () => {
   return (
     <>
       <Header />
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route
+          exact
+          path="/"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Home />
+            </Suspense>
+          }
+        />
         <Route
           exact
           path="/discover/genre=:genre/sort_by=:sortValue/page/:page"
-          element={<Movies />}
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Movies />
+            </Suspense>
+          }
         />
-        <Route exact path="/m/details/:id" element={<MovieDetails />} />
-        <Route exact path="/p/details/:id" element={<PeopleDetails />} />
-        <Route path="*" element={<NotFound />} />
+
+        <Route
+          exact
+          path="/m/details/:id"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <MovieDetails />
+            </Suspense>
+          }
+        />
+        <Route
+          exact
+          path="/p/details/:id"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <PeopleDetails />
+            </Suspense>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <NotFound />
+            </Suspense>
+          }
+        />
       </Routes>
     </>
   );
